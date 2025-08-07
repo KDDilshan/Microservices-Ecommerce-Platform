b248f06b6205ff12805a65f41919a62643a3972f
KDDilshan <kdprojects847@gmail.com>
2025-08-07 18:47:58 +0530

    add: product controller for manage endpoints

---
+++ services/product/src/main/java/com/kavindu/ecommerce/product/ProductController.java
@@ -0,0 +1,43 @@
+package com.kavindu.ecommerce.product;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Integer> createProduct(
+            @RequestBody @Valid ProductRequest request
+    ){
+        return ResponseEntity.ok(productService.createProduct(request));
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<List<ProductPurchaseResponse>> purchaseProducts(
+            @RequestBody List<ProductPurchaseRequest> requests
+    ){
+        return ResponseEntity.ok(productService.purchaseProducts(requests));
+    }
+
+    @GetMapping("/{product-id}")
+    public ResponseEntity<ProductResponse> findById(
+            @PathVariable("product-id") Integer productId
+    ) {
+        return ResponseEntity.ok(productService.findById(productId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> findAll() {
+        return ResponseEntity.ok(productService.findAll());
+    }
+}
