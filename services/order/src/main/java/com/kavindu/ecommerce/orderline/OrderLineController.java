fd3cdac6d385dd634ef67224bd47f70d15e7cc15
KDDilshan <kdprojects847@gmail.com>
2025-08-09 19:11:53 +0530

    contoler for orderline endpoints

---
+++ services/order/src/main/java/com/kavindu/ecommerce/orderline/OrderLineController.java
@@ -0,0 +1,31 @@
+package com.kavindu.ecommerce.orderline;
+
+import com.kavindu.ecommerce.order.OrderResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/order-lines")
+public class OrderLineController {
+
+    private final OrderLineService service;
+
+    public OrderLineController(OrderLineService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/order/{order-id}")
+    public ResponseEntity<List<OrderLineResponse>> findByOrderId(
+            @PathVariable("order-id") Integer orderId
+    ){
+        return ResponseEntity.ok(service.findAllByOrderId(orderId));
+    }
+
+
+}
